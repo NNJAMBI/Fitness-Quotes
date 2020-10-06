9f95590e5713dc7e1795c228538703009f661db0
Nancy Njambi Karanja <nnjambi@safaricom.co.ke>
2020-10-07 00:47:44 +0300

    add add new quote component

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,14 +9,21 @@ import { Quote } from '../quote';
 
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
-    new Quote(1, 'Thomas Jefferson - Push yourself!', 'If you want something you’ve never had, you must be willing to do something you’ve never done', new Date(2020,8,22)),
-    new Quote(2, 'Muhammad Ali - Dont count days!', 'Nothing will work unless you do', new Date(2019,4,27)),
-    new Quote(3, 'Maya Angelou - Do!', 'Don’t count the days, make the days count', new Date(2030,7,9)),
-    new Quote(4, 'Mahatma Gandhi - Strength!', 'Strength does not come from physical capacity. It comes from an indomitable will', new Date(2020,10,11)),
+    new Quote(1, 'Thomas Jefferson - Push yourself!', 'If you want something you’ve never had, you must be willing to do something you’ve never done',  new Date(2020,8,22), 'admin' ),
+    new Quote(2, 'Muhammad Ali - Dont count days!', 'Nothing will work unless you do', new Date(2019,4,27), 'admin'),
+    new Quote(3, 'Maya Angelou - Do!', 'Don’t count the days, make the days count', new Date(2030,7,9), 'admin'),
+    new Quote(4, 'Mahatma Gandhi - Strength!', 'Strength does not come from physical capacity. It comes from an indomitable will', new Date(2020,10,11), 'admin'),
   ];
 
 toggleDetails(index) {
   this.quotes[index].showDescription = !this.quotes[index].showDescription;
+}
+
+addNewQuote(quote) {
+  let quoteLength = this.quotes.length;
+  quote.id = quoteLength + 1;
+  quote.completeDate = new Date(quote.completeDate)
+  this.quotes.push(quote)
 }
 
 completeQuote(isComplete, index){
