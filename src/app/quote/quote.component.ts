dc158c31da06f207fc5f73815b9f0bac1042d25d
Nancy Njambi Karanja <nnjambi@safaricom.co.ke>
2020-10-07 09:33:41 +0300

    add fix for delete quote function

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -31,7 +31,7 @@ completeQuote(isComplete, index){
     this.quotes.splice(index,1);
   }
 }
-deletequote(isComplete, index){
+quoteDelete(isComplete, index){
   if (isComplete) {
     let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}?`)
 
